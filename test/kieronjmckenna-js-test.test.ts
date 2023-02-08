55862e84fb037fbefbbcb9f02750273be7b4f6c2
focusotter <mtliendo@focusotter.com>
2023-02-08 10:08:05 -0600

    Initial commit

---
+++ test/kieronjmckenna-js-test.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as KieronjmckennaJsTest from '../lib/kieronjmckenna-js-test-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/kieronjmckenna-js-test-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new KieronjmckennaJsTest.KieronjmckennaJsTestStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
